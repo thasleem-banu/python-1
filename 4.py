277ab81ce4a5494d9b747e19f785eaef4e06e589
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-06-25 15:27:08 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+l=input()
+a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','c','D','E','F','G','H','I','J','K''L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+if l in a:
+   print("Alphabet")
+else:
+   print("No")
