59e808e8c2d203cd893451ff2d3b02a6f05d1e10
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-06-27 11:12:09 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,9 +1,9 @@
 n=int(input())
 if n>1: 
    for i in range(2,3):
-      if(n%1==0):
-         print("yes")
-      else:
+      if(n%i==0):
          print("no")
+      else:
+         print("yes")
 else:
    print("no")
